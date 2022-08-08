62c4580ea8a489394abd0c992f87d454283537c8
lilibenedikta <55950892+lilibenedikta@users.noreply.github.com>
2022-08-08 20:54:49 +0200

    change font

--- pysankey/sankey.py
+++ pysankey/sankey.py
@@ -97,8 +97,9 @@ def sankey(left, right, leftWeight=None, rightWeight=None, colorDict=None,
 
     plt.figure()
     plt.rc('text', usetex=False)
-    plt.rc('font', family='monospace')
-    #plt.rc('font',**{'family':'serif','serif':['Times New Roman']})
+    #plt.rc('font', family='monospace')
+    matplotlib.rcParams['font.serif'] = 'Times New Roman'  
+    #plt.rc('font',**{'family':'serif','serif':['Times']})
 
     # Create Dataframe
     if isinstance(left, pd.Series):
